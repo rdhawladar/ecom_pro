6189e44eba71c464acbea3f803603c25af7550f9
RD Hawladar <rdhawladar@gmail.com>
2018-06-29 22:07:55 +0600

    login modal completed

--- header.php
+++ header.php
@@ -20,6 +20,7 @@
 					<li><a href="order.php">Orders</a></li>
 					<li><a href="request.php">Request</a></li>
 					<li><a href="#">Setting</a></li>
+					<li><a href="#" data-toggle="modal" data-target="#loginModal">Log In</a></li>
 				</ul>
 				<ul class="nav navbar-nav navbar-right">
 					<li><a href="cart.php"><img src="images/cart.svg"></a></li>
@@ -37,3 +38,47 @@
 			</div><!-- /.navbar-collapse -->
 		</div><!-- /.container-fluid -->
 	</nav>
+
+<!-- Login Modal -->
+<div class="modal fade bs-example-modal-lg" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+      	<div class="row">
+      		<div class="col-md-6 padding-0">
+      			<img class="img-responsive img-right" src="https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg" >
+      		</div>
+      		<div class="col-md-6">
+      			<div class="right-half">
+	      				
+	      			
+		        	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> <br><br>
+		      		<h4 class="login-title">Someone is waiting ...<br> Start helping him by Log in</h4>
+					<form class="form-horizontal">
+					  <div class="form-group">
+					    <div class="col-sm-12">
+					      <input type="email" class="form-control" id="inputEmail3" placeholder="email/username">
+					    </div>
+					  </div>
+					  <div class="form-group">
+					    <div class="col-sm-12">
+					      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+					    </div>
+					  </div>
+					  <button type="submit" class="btn btn-login">Login</button>
+					</form>
+					<div class="social-icon-modal">
+						<p>Social Login: </p>
+						<img src="images/social/twitter.svg">
+						<img src="images/social/instagram.svg">
+						<img src="images/social/facebook.svg">
+					</div>
+					<p class="modal-bottom"><a href="#">Forget Password </a> | <a href="#">Sign Up</a></p>
+      			</div>
+      		</div>
+      	</div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- !END Login Modal  -->
