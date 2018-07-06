0dd1e53c1ce8fa43fac13a7d34e9a1dd133f159d
RD Hawladar <rdhawladar@gmail.com>
2018-07-07 02:42:23 +0600

    payment confimation page completed

--- header.php
+++ header.php
@@ -18,6 +18,7 @@
 					<li><a href="#">Shop</a></li>
 					<li><a href="travel.php">Add Travel</a></li>
 					<li><a href="order.php">Orders</a></li>
+					<li><a href="payment_confirmation.php">Payment Confirmation</a></li>
 					<li><a href="request.php">Request</a></li>
 					<li><a href="#">Setting</a></li>
 					<li><a href="#" data-toggle="modal" data-target="#loginModal">Log In</a></li>
