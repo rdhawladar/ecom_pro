46513f38702ef79fe3f926ed807da16085278518
ri-webhawksit <34907283+ri-webhawksit@users.noreply.github.com>
2018-07-20 11:32:59 +0600

    withdraw setting modal

--- setting-payout.php
+++ setting-payout.php
@@ -9,7 +9,7 @@
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	
   <link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="css/setting-payout.css">
+	<link rel="stylesheet" href="css/travel.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 	<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
@@ -71,5 +71,49 @@
         </div>
       </div>
 
+
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Payout Confirmation</h5>
+      </div>
+      <div class="modal-body">
+                    <div class="row layer-4">
+                          <div class="col-md-12">
+                                <table class="table table-responsive table-borderless" >
+                                  <tr>
+                                    <td>Payout Amount</td>
+                                    <td>$5042</td>
+                                    <td rowspan="7"><img src="images/demo.jpg" class="img-responsive"></td>
+                                  </tr>
+                                  <tr>
+                                    <td>Withdraw Method</td>
+                                    <td>PP-Booth</td>
+                                  </tr>
+                                  <tr>
+                                    <td>Booth Id</td>
+                                    <td>DD5674 (Dresden Airport, Germany - DRS )</td>
+                                  </tr>
+                                </table>
+                          </div>
+                          
+                        
+                    </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-common btn-round" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-common btn-round">Confirm</button>
+      </div>
+    </div>
+  </div>
+</div>
 </body>
 </html>
